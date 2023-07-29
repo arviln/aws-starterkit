9f909f5bf71b953e66ee1dc28de1e6fef69162ae
arvil <arvil@abbey.so>
2023-07-28 17:36:25 -0700

    push group 2

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 }
 
 resource "abbey_grant_kit" "IAM_membership" {
-  name = "IAM_membership"
+  name = "IAM_membership_2"
   description = <<-EOT
     Grants membership to an IAM Group.
     This Grant Kit uses a single-step Grant Workflow that requires only a single reviewer
