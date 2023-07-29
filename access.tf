aa0f85e588814b6ff4be9c9218e312c441b66824
Abbey <no-reply@abbey.io>
2023-07-29 00:38:07 +0000

    arvil+test1@abbey.io requests access to: IAM_membership_2

--- access.tf
+++ access.tf
@@ -2,3 +2,7 @@ resource "aws_iam_user_group_membership" "user_quickstart-user_group_aws-quickst
   user = "quickstart-user"
   groups = ["aws-quickstart-test"]
 }
+resource "aws_iam_user_group_membership" "user_quickstart-user_group_aws-quickstart-test2" {
+  user = "quickstart-user"
+  groups = ["aws-quickstart-test2"]
+}
